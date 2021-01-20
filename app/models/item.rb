66e49fbbbc9656a30c81cbf45f5645166a191ee1
ackey-getup <akira.getup@gmail.com>
2021-01-20 20:01:55 +0900

    同一バリデーションの括り・不要なファイルの削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,14 +9,18 @@ class Item < ApplicationRecord
   belongs_to :region
   belongs_to :mean
 
-  validates :image,       presence: { message: "画像を選択してください"}
-  validates :title,       presence: { message: "商品名を入力してください"}
-  validates :price,       presence: { message: "価格を設定してください"}, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "価格は半角数字で¥300~¥9,999,999の間で入力してください" }
-  validates :explanation, presence: { message: "商品の説明文を記載してください"}
-  validates :category_id, numericality: { other_than: 1 , message: "カテゴリーを選択してください" }
-  validates :status_id,   numericality: { other_than: 1 , message: "商品の状態を選択してください" }
-  validates :delivery_id, numericality: { other_than: 1 , message: "配送料の負担を選択してください" }
-  validates :region_id,   numericality: { other_than: 1 , message: "発送元の地域を選択してください" }
-  validates :mean_id,     numericality: { other_than: 1 , message: "発送までの日数を選択してください" }
+  with_options presence: true do
+    validates :image,       message: "画像を選択してください"
+    validates :title,       message: "商品名を入力してください"
+    validates :price,       message: "価格を設定してください", numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "価格は半角数字で¥300~¥9,999,999の間で入力してください" }
+    validates :explanation, message: "商品の説明文を記載してください"
+  end
 
+  with_options numericality: other_than: 1 do
+    validates :category_id, message: "カテゴリーを選択してください"
+    validates :status_id,   message: "商品の状態を選択してください"
+    validates :delivery_id, message: "配送料の負担を選択してください"
+    validates :region_id,   message: "発送元の地域を選択してください"
+    validates :mean_id,     message: "発送までの日数を選択してください"
+  end
 end
--- spec/factories/means.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :mean do
-    
-  end
-end
