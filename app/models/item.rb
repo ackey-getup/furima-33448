b83c3098251c3deeb7099ff7ee2317a6bb1a4676
ackey-getup <akira.getup@gmail.com>
2021-01-21 17:49:05 +0900

    with_optionsでバリデーション括り・テストコードの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,17 +10,18 @@ class Item < ApplicationRecord
   belongs_to :mean
 
   with_options presence: true do
-    validates :image,       message: "画像を選択してください"
-    validates :title,       message: "商品名を入力してください"
-    validates :price,       message: "価格を設定してください", numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "価格は半角数字で¥300~¥9,999,999の間で入力してください" }
-    validates :explanation, message: "商品の説明文を記載してください"
+    validates :image 
+    validates :title 
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "価格は半角数字で¥300~¥9,999,999の間で入力してください" }
+    validates :explanation
   end
 
-  with_options numericality: other_than: 1 do
-    validates :category_id, message: "カテゴリーを選択してください"
-    validates :status_id,   message: "商品の状態を選択してください"
-    validates :delivery_id, message: "配送料の負担を選択してください"
-    validates :region_id,   message: "発送元の地域を選択してください"
-    validates :mean_id,     message: "発送までの日数を選択してください"
+  with_options numericality: { other_than: 1, message: '選択してください' } do 
+    validates :category_id
+    validates :status_id
+    validates :delivery_id
+    validates :region_id
+    validates :mean_id
   end
+
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -19,67 +19,115 @@ RSpec.describe Item, type: :model do
       it '商品画像がなければ出品できない' do
         @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('Image 画像を選択してください')
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
 
       it '商品名が空では出品できない' do
         @item.title = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('Title 商品名を入力してください')
+        expect(@item.errors.full_messages).to include("Title can't be blank")
       end
 
       it '商品の説明が空では出品できない' do
         @item.explanation = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('Explanation 商品の説明文を記載してください')
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
 
       it 'カテゴリーの情報が無選択では出品できない' do
         @item.category_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('Category カテゴリーを選択してください')
+        expect(@item.errors.full_messages).to include("Category 選択してください")
       end
 
       it '商品の状態についての情報が無選択では出品できない' do
         @item.status_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('Status 商品の状態を選択してください')
+        expect(@item.errors.full_messages).to include("Status 選択してください")
       end
       
       it '配送料の負担についての情報が無選択では出品できない' do
         @item.delivery_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('Delivery 配送料の負担を選択してください')
+        expect(@item.errors.full_messages).to include("Delivery 選択してください")
       end
 
       it '発送元の地域についての情報が無選択では出品できない' do
         @item.region_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('Region 発送元の地域を選択してください')
+        expect(@item.errors.full_messages).to include("Region 選択してください")
       end
 
       it '発送までの日数についての情報が無選択では出品できない' do
         @item.mean_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('Mean 発送までの日数を選択してください')
+        expect(@item.errors.full_messages).to include("Mean 選択してください")
       end
 
       it '価格についての情報が空では出品できない' do
         @item.price = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price 価格を設定してください')
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
       it '価格の範囲が、¥300~¥9,999,999の間でなければ出品できない' do
         @item.price = 100
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price 価格は半角数字で¥300~¥9,999,999の間で入力してください')
+        expect(@item.errors.full_messages).to include("Price 価格は半角数字で¥300~¥9,999,999の間で入力してください")
       end
 
       it '販売価格は半角数字でなければ出品できない' do
         @item.price = "３００"
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price 価格は半角数字で¥300~¥9,999,999の間で入力してください')
+        expect(@item.errors.full_messages).to include("Price 価格は半角数字で¥300~¥9,999,999の間で入力してください")
+      end
+      
+      it 'priceは半角英数混合では登録できないこと' do
+        @item.price = "300abc"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price 価格は半角数字で¥300~¥9,999,999の間で入力してください")
+      end
+
+      it 'priceは半角英語だけでは登録できないこと' do
+        @item.price = "price"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price 価格は半角数字で¥300~¥9,999,999の間で入力してください")
+      end
+
+      it 'priceは10,000,000以上では登録できないこと' do
+        @item.price = "100000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price 価格は半角数字で¥300~¥9,999,999の間で入力してください")
+      end
+
+      it '発送までの日数についての情報が1以外でないと登録できないこと' do
+        @item.mean_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Mean 選択してください")
+      end
+
+      it '発送元の地域についての情報が1以外でないと登録できないこと' do
+        @item.region_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Region 選択してください")
+      end
+
+      it 'カテゴリーの情報が1以外でないと登録できないこと' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category 選択してください")
+      end
+
+      it '配送料の負担についての情報が1以外でないと登録できないこと' do
+        @item.delivery_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery 選択してください")
+      end
+
+      it '商品の状態についての情報が1以外でないと登録できないこと' do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status 選択してください")
       end
 
     end
