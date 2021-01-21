a4f3acbbe5527c92dcc98f0fe157d886aa491bfa
ackey-getup <akira.getup@gmail.com>
2021-01-21 19:59:03 +0900

    マイグレーションファイルの修正

--- db/migrate/20210118032245_create_items.rb
+++ db/migrate/20210118032245_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string     :title,       null: false
       t.integer    :price,       null: false
-      t.text       :, null: false
+      t.text       :explanation, null: false
       t.references :user,        null: false, foreign_keys: true
       t.integer    :category_id, null: false
       t.integer    :status_id,   null: false
