3587f45d4f03a675fb5dc22f2d718606634d357e
ackey-getup <akira.getup@gmail.com>
2021-01-22 12:21:34 +0900

    商品詳細ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
 #  def destroy
 #    @item.destroy
 #    redirect_to root_path
